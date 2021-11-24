1aab823bd2822fa772dc61eb41c625d2ff59bc05
mivasil6 <mivasil6@mts.ru>
2021-11-24 09:56:56 +0300

    [ONE-355] fix wait-for-it.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
-./wait-for-it.sh -h "${ONETL_HIVE_CONN_HOST}" -p "${ONETL_HIVE_CONN_PORT}" -t 0;
+/wait-for-it.sh -h "${ONETL_HIVE_CONN_HOST}" -p "${ONETL_HIVE_CONN_PORT}" -t 0;
 
 exec "$@"
