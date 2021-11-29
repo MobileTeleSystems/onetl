d68b929c305d4980d1676a9634d40d57d16f07d9
Абрамова Анна Юрьевна <ayabram8@mts.ru>
2021-11-29 11:09:25 +0000

    [ONE-317] fix .gitlab-ci.yml

--- docker/postgres/init-users.sh
+++ docker/postgres/init-users.sh
@@ -8,7 +8,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE "metastore2_db" OWNER = hive2;
     GRANT ALL PRIVILEGES ON DATABASE "metastore2_db" TO hive2;
 
-    CREATE ROLE onetl WITH LOGIN PASSWORD 'onetl';
-    CREATE DATABASE "onetl" OWNER = onetl;
-    GRANT ALL PRIVILEGES ON DATABASE "onetl" TO onetl;
 EOSQL
