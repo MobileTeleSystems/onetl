557bb14581b2207b9124f27191e88133941ebb2d
mivasil6 <mivasil6@mts.ru>
2021-12-09 11:50:41 +0300

    [ONE-418] remove sharepoint code

--- onetl/connection/file_connection/__init__.py
+++ onetl/connection/file_connection/__init__.py
@@ -2,6 +2,5 @@ from onetl.connection.file_connection.file_connection import FileConnection
 from onetl.connection.file_connection.ftp_connection import FTP
 from onetl.connection.file_connection.ftps_connection import FTPS
 from onetl.connection.file_connection.sftp_connection import SFTP
-from onetl.connection.file_connection.sharepoint_connection import Sharepoint
 from onetl.connection.file_connection.samba_connection import Samba
 from onetl.connection.file_connection.hdfs_connection import HDFS
