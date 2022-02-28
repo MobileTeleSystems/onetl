64699fef2561a5405fb9b8ea8502d1e207c24707
Абрамова Анна Юрьевна <ayabram8@mts.ru>
2022-02-28 08:38:57 +0000

    Resolve ONE-694 "Feature/"

--- version.py
+++ version.py
@@ -7,5 +7,5 @@ def get_version():
     if "CI_COMMIT_TAG" in os.environ:
         return os.environ["CI_COMMIT_TAG"]
 
-    build_num = os.environ.get("CI_PIPELINE_ID", "")
+    build_num = os.environ.get("CI_PIPELINE_IID", "")
     return f"{version}.dev{build_num}"
