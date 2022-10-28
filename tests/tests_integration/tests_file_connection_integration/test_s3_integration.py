8d4828da94b71f4f949f205b9c0e34a24813f18e
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2022-10-28 10:17:44 +0000

    [ONE-1091] add tests for FileConnection. Methods path_exists, is_dir, is_file

--- tests/tests_integration/tests_file_connection_integration/test_s3_integration.py
+++ tests/tests_integration/tests_file_connection_integration/test_s3_integration.py
@@ -10,27 +10,6 @@ def test_s3_connection_check(caplog, s3):
 
 
 @pytest.mark.S3
-def test_s3_connection_path_exists(s3):
-    s3.client.fput_object(
-        "testbucket",
-        "export/resources/src/exclude_dir/file_4.txt",
-        "tests/resources/src/exclude_dir/file_4.txt",
-    )
-    assert s3.path_exists("export/resources/src/exclude_dir")
-    assert not s3.path_exists("export/resources/src/exclude_dir_32")
-
-
-@pytest.mark.S3
-def test_s3_connection_is_dir(s3):
-    s3.client.fput_object(
-        "testbucket",
-        "export/resources/src/exclude_dir/file_4.txt",
-        "tests/resources/src/exclude_dir/file_4.txt",
-    )
-    assert s3.is_dir("export")
-
-
-@pytest.mark.S3
 def test_s3_connection_list_dir(s3):
     s3.client.fput_object(
         "testbucket",
@@ -47,14 +26,3 @@ def test_s3_connection_list_dir(s3):
         "file_4.txt",
         "file_5.txt",
     ]
-
-
-@pytest.mark.S3
-def test_s3_connection_is_file(s3):
-    s3.client.fput_object(
-        "testbucket",
-        "export/resources/src/exclude_dir/file_4.txt",
-        "tests/resources/src/exclude_dir/file_4.txt",
-    )
-    assert s3.is_file("export/resources/src/exclude_dir/file_4.txt")
-    assert not s3.is_file("export/resources/src/exclude_dir")
