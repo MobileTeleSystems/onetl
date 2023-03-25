db6779798c59b8af844d33388affb10c80b4225d
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2023-03-25 11:43:00 +0000

    [DOP-2666] Remove test plugins after exiting pytest

--- run_tests.sh
+++ run_tests.sh
@@ -8,4 +8,10 @@ python3 -m pip uninstall -y dummy failing || true
 python3 -m pip install -e $root_path/tests/libs/dummy
 python3 -m pip install -e $root_path/tests/libs/failing
 
+set +e
+
 $root_path/pytest_runner.sh "$@"
+ret=$?
+
+python3 -m pip uninstall -y dummy failing || true
+exit "$ret"
