c2bf23c72f11f24b3223a1023c7b05b003d2da7b
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2023-03-25 11:42:15 +0000

    [DOP-2666] Rewtire MongoDB where and hint tests to use DBReader

--- tests/tests_integration/tests_core_integration/tests_db_reader_integration/test_mongodb_reader_integration.py
+++ tests/tests_integration/tests_core_integration/tests_db_reader_integration/test_mongodb_reader_integration.py
@@ -46,3 +46,69 @@ def test_mongodb_reader_snapshot(spark, processing, load_table_data):
         table=load_table_data.table,
         df=df,
     )
+
+
+def test_mongodb_reader_snapshot_with_where(spark, processing, load_table_data):
+    mongo = MongoDB(
+        host=processing.host,
+        port=processing.port,
+        user=processing.user,
+        password=processing.password,
+        database=processing.database,
+        spark=spark,
+    )
+
+    reader = DBReader(
+        connection=mongo,
+        table=load_table_data.table,
+        df_schema=df_schema,
+    )
+    table_df = reader.run()
+
+    reader1 = DBReader(
+        connection=mongo,
+        table=load_table_data.table,
+        df_schema=df_schema,
+        hint={"_id": 1},
+        where={"_id": {"$lt": 1000}},
+    )
+
+    table_df1 = reader1.run()
+
+    assert table_df1.count() == table_df.count()
+
+    reader2 = DBReader(
+        connection=mongo,
+        table=load_table_data.table,
+        df_schema=df_schema,
+        where={"$or": [{"_id": {"$lt": 1000}}, {"_id": {"$eq": 1000}}]},
+    )
+    table_df2 = reader2.run()
+
+    assert table_df2.count() == table_df.count()
+
+    processing.assert_equal_df(
+        schema=load_table_data.schema,
+        table=load_table_data.table,
+        df=table_df1,
+    )
+
+    one_reader = DBReader(
+        connection=mongo,
+        table=load_table_data.table,
+        df_schema=df_schema,
+        where={"_id": {"$eq": 50}},
+    )
+    one_df = one_reader.run()
+
+    assert one_df.count() == 1
+
+    empty_reader = DBReader(
+        connection=mongo,
+        table=load_table_data.table,
+        df_schema=df_schema,
+        where={"_id": {"$gt": 1000}},
+    )
+    empty_df = empty_reader.run()
+
+    assert not empty_df.count()
