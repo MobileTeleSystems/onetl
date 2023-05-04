8fe919a9e4de0fe7f70544efe3bf71d6265b7e75
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2023-05-04 15:37:34 +0000

    [DOP-4663] Move default validate_hwm_expression to a mixin

--- onetl/connection/db_connection/jdbc_connection.py
+++ onetl/connection/db_connection/jdbc_connection.py
@@ -29,6 +29,7 @@ from onetl.connection.db_connection.dialect_mixins import (
     SupportColumnsList,
     SupportDfSchemaNone,
     SupportHintStr,
+    SupportHWMExpressionStr,
     SupportWhereStr,
 )
 from onetl.connection.db_connection.dialect_mixins.support_table_with_dbschema import (
@@ -144,6 +145,7 @@ class JDBCConnection(SupportDfSchemaNone, JDBCMixin, DBConnection):  # noqa: WPS
         SupportDfSchemaNone,
         SupportWhereStr,
         SupportHintStr,
+        SupportHWMExpressionStr,
         DBConnection.Dialect,
     ):
         pass
