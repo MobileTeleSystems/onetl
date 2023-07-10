f24e66488b8232ed9a5d5a9418c6ac813026acc7
Pedchenko Dmitry <dypedchenk@mts.ru>
2023-07-10 13:11:39 +0300

    [DOP-2329] Generate config for authorization in Kafka

--- tests/tests_integration/tests_db_connection_integration/test_kafka_integration.py
+++ tests/tests_integration/tests_db_connection_integration/test_kafka_integration.py
@@ -8,6 +8,7 @@ from onetl.connection import Kafka
 
 def test_kafka_connection_get_jaas_conf_deploy_keytab_true(spark, create_keytab):
     # Arrange
+    cloned_keytab = Path("./keytab")
     # deploy_keytab=True by default
     kafka = Kafka(
         spark=spark,
@@ -17,11 +18,12 @@ def test_kafka_connection_get_jaas_conf_deploy_keytab_true(spark, create_keytab)
         keytab=create_keytab,
     )
 
+    assert not cloned_keytab.exists()
+
     # Act
     kafka._get_jaas_conf()
 
     # Assert
-    cloned_keytab = Path("./keytab")
     assert cloned_keytab.exists()
     assert cloned_keytab.read_text() == create_keytab.read_text()
     cloned_keytab.unlink()
