02bd356cbacc856704f78928f4f8471d2490adf3
maxim-lixakov <lixakov.maksim@yandex.ru>
2023-09-22 15:05:23 +0300

    [DOP-8838] - change parameters for starting samba container

--- docker/samba/on_post_init.sh
+++ docker/samba/on_post_init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # allow create files and directories
-chmod 777 /share/folder
+mkdir -p /share/folder
+chmod 0777 /share/folder
 /entrypoint.sh -u "1000:1000:onetl:onetl:awd123fd1" -s "SmbShare:/share/folder:rw:onetl"
