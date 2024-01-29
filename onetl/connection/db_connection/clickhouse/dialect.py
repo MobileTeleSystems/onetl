e2afd3d6abefd8c3b04dd4d29d4b6ab591305a5c
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-01-29 13:31:06 +0000

    [DOP-11906] Remove min/max workaround for Clickhouse

--- onetl/connection/db_connection/clickhouse/dialect.py
+++ onetl/connection/db_connection/clickhouse/dialect.py
@@ -15,6 +15,7 @@
 from __future__ import annotations
 
 from datetime import date, datetime
+from typing import Any
 
 from onetl.connection.db_connection.jdbc_connection import JDBCDialect
 
@@ -25,6 +26,16 @@ class ClickhouseDialect(JDBCDialect):
 
     def get_partition_column_mod(self, partition_column: str, num_partitions: int) -> str:
         return f"{partition_column} % {num_partitions}"
+
+    def get_max_value(self, value: Any) -> str:
+        # Max function in Clickhouse returns 0 instead of NULL for empty table
+        result = self._serialize_value(value)
+        return f"maxOrNull({result})"
+
+    def get_min_value(self, value: Any) -> str:
+        # Min function in Clickhouse returns 0 instead of NULL for empty table
+        result = self._serialize_value(value)
+        return f"minOrNull({result})"
 
     def _serialize_datetime(self, value: datetime) -> str:
         result = value.strftime("%Y-%m-%d %H:%M:%S")
