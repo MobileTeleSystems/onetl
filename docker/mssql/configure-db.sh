c757b7089f4930811f3bf2dc0a356c7a3e9673f3
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-07-29 11:09:11 +0000

    Fix nightly tests

--- docker/mssql/configure-db.sh
+++ docker/mssql/configure-db.sh
@@ -19,7 +19,7 @@ while true; do
         exit 1
     fi
 
-    DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P ${MSSQL_SA_PASSWORD} -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases" 2>/dev/null | sed -e 's/^[[:space:]]*//')
+    DBSTATUS=$(sqlcmd -h -1 -t 1 -U sa -P ${MSSQL_SA_PASSWORD} -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases" 2>/dev/null | sed -e 's/^[[:space:]]*//')
     ERRCODE=$?
     if [[ "$DBSTATUS" -eq "0" && "$ERRCODE" -eq "0" ]]; then
         echo "INFO: Database ready."
@@ -32,5 +32,5 @@ done
 
 # Run the setup script to create the DB and the schema in the DB
 echo "Running setup.sql";
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /usr/config/setup.sql;
+sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /usr/config/setup.sql;
 echo "Success";
