d1678108d965d9e17945c2f2baec78c4367b79e7
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-03-17 14:48:35 +0000

    [DOP-22434] Improve Avro documentation

--- docs/file_df/file_formats/avro.rst
+++ docs/file_df/file_formats/avro.rst
@@ -6,4 +6,5 @@ Avro
 .. currentmodule:: onetl.file.format.avro
 
 .. autoclass:: Avro
-    :members: get_packages, parse_column, serialize_column
+    :members: get_packages, parse_column, serialize_column, schema_dict, schema_url, recordName,recordNamespace,compression,mode,datetimeRebaseMode,positionalFieldMatching,enableStableIdentifiersForUnionType
+    :member-order: bysource
--- tests/tests_integration/test_file_format_integration/test_avro_integration.py
+++ tests/tests_integration/test_file_format_integration/test_avro_integration.py
@@ -72,7 +72,7 @@ def test_avro_reader(
 
     reader = FileDFReader(
         connection=local_fs,
-        format=Avro(schema_dict=avro_schema, **options),
+        format=Avro(avroSchema=avro_schema, **options),
         df_schema=df.schema,
         source_path=avro_root,
     )
@@ -109,7 +109,7 @@ def test_avro_writer(
 
     writer = FileDFWriter(
         connection=file_df_connection,
-        format=Avro(schema_dict=avro_schema, **options),
+        format=Avro(avroSchema=avro_schema, **options),
         target_path=avro_root,
     )
     writer.run(df)
@@ -151,7 +151,7 @@ def test_avro_serialize_and_parse_column(
         context_manager = contextlib.nullcontext()
 
     df = file_df_dataframe
-    avro = Avro(schema_dict=avro_schema)
+    avro = Avro(avroSchema=avro_schema)
 
     combined_df = df.withColumn("combined", struct([col(c) for c in df.columns]))
 
@@ -194,7 +194,7 @@ def test_avro_serialize_and_parse_no_schema(
 
         with pytest.raises(
             ValueError,
-            match="Avro.parse_column can be used only with defined `schema_dict` or `schema_url`",
+            match="Avro.parse_column can be used only with defined `avroSchema` or `avroSchemaUrl`",
         ):
             serialized_df.select(avro.parse_column(column_type("combined")))
 
@@ -228,7 +228,7 @@ def test_avro_serialize_and_parse_with_schema_url(
     responses.add(responses.GET, schema_url, json=avro_schema, status=200)
 
     df = file_df_dataframe
-    avro = Avro(schema_url=schema_url)
+    avro = Avro(avroSchemaUrl=schema_url)
 
     combined_df = df.withColumn("combined", struct([col(c) for c in df.columns]))
     with context_manager:
@@ -236,3 +236,42 @@ def test_avro_serialize_and_parse_with_schema_url(
         assert isinstance(serialized_df.schema["combined"].dataType, BinaryType)
         parsed_df = serialized_df.select(avro.parse_column(column_type("combined")))
         assert combined_df.select("combined").collect() == parsed_df.collect()
+
+
+def test_avro_serialize_and_parse_column_unsupported_options_warning(
+    spark,
+    file_df_dataframe,
+    avro_schema,
+):
+    from pyspark.sql.functions import struct
+
+    spark_version = get_spark_version(spark)
+    if spark_version < Version("3"):
+        pytest.skip("Avro.parse_column` or `Avro.serialize_column` are available only since Spark 3.x")
+
+    df = file_df_dataframe
+
+    combined_df = df.withColumn("combined", struct([col(c) for c in df.columns]))
+
+    avro_serialize = Avro(
+        avroSchema=avro_schema,
+        compression="gzip",
+        mode="PERMISSIVE",
+        positionalFieldMatching=True,
+        enableStableIdentifiersForUnionType=True,
+    )
+    msg = (
+        "Options `['compression', 'enableStableIdentifiersForUnionType', 'mode', 'positionalFieldMatching']` "
+        "are set but not supported by `Avro.serialize_column`."
+    )
+    with pytest.warns(UserWarning) as record:
+        serialized_df = combined_df.select(avro_serialize.serialize_column(combined_df.combined))
+        assert record
+        assert msg in str(record[0].message)
+
+    avro_parse = Avro(avroSchema=avro_schema, compression="gzip")
+    msg = "Options `['compression']` are set but not supported by `Avro.parse_column`."
+    with pytest.warns(UserWarning) as record:
+        serialized_df.select(avro_parse.parse_column(serialized_df.combined))
+        assert record
+        assert msg in str(record[0].message)
